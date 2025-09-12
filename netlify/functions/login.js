dfa17b1e7894f268015b72b810a2437182d26fa6
IndexCoderong <148581158+tacticalIndex@users.noreply.github.com>
2025-09-12 17:52:15 -0500

    Update login.js

--- netlify/functions/login.js
+++ netlify/functions/login.js
@@ -1,6 +1,6 @@
 export async function handler(event) {
   const CLIENT_ID = process.env.DISCORD_APPLICATION_CLIENT_ID;
-  const REDIRECT_URI = "https://ariv-staff-activity-logger.netlify.app/.netlify/functions/callback";
+  const REDIRECT_URI = "https://a55staffshiftmanagement.netlify.app/.netlify/functions/callback";
   const scopes = ["identify", "guilds", "guilds.members.read"];
 
   const pageKey = event.queryStringParameters.pageKey;
